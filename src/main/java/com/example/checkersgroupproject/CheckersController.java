0ebae719e14e4fbd0051230e78d0ae054a9b3b08
sethnans <seth.nans@yahoo.com>
2025-04-15 14:52:49 -0400

    Merge pull request #2 from sethnans/copy
    copy over files

--- src/main/java/com/example/checkersgroupproject/CheckersController.java
+++ src/main/java/com/example/checkersgroupproject/CheckersController.java
@@ -1,4 +1,25 @@
 package com.example.checkersgroupproject;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
 public class CheckersController {
+    @FXML
+    private Label boardLabel;
+
+    @FXML
+    private Label movesLabel;
+
+    @FXML
+    private TextField inputTextField;
+
+
 }
